bc611cbaa264784b6ffcad0d5fa77b46ade4a276
rodribp <rodrigobonilla177@gmail.com>
2024-04-04 23:24:45 -0600

    adding product viewer component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import "@/app/page.css";
+import ProductsViewer from "@/app/components/productsViewer";
 
 export default function Home() {
   return (
     <>
-      <header className="block align-middle lg:flex max-w-screen-2xl h-1/2 items-center justify-between">
+      <header className="block mb-20 align-middle lg:flex max-w-screen-2xl h-1/2 items-center justify-between">
         <img
           className="phone-shado lg:hidden w-48"
           src="/img/phone.webp"
           alt="Phone img"
         />
-        <div className="block lg:pt-48">
+        <div className="block lg:pt-22">
           <h1 className="text-7xl font-bold">The best products</h1>
           <h2 className=" mt-4 text-3xl">For the best customers</h2>
         </div>
@@ -20,6 +21,7 @@ export default function Home() {
           alt="Phome img"
         />
       </header>
+      <ProductsViewer />
     </>
   );
 }
